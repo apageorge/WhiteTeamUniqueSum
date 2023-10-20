7a577cde463c8eec1c7b697f2f2ca577cffd6d27
Aparna <aparna@qa.com>
2023-10-20 14:36:51 +0100

    Unique sum - version 1- Implementation with HashMap

---
+++ src/UniqueSum.java
@@ -0,0 +1,23 @@
+import java.util.HashMap;
+
+
+public class UniqueSum {
+    public static int findUniqueSum(int[] nums){
+        HashMap<Integer,Integer> numMap = new HashMap<Integer,Integer>();
+        int sum =0;
+        for (int i : nums){
+            if (numMap.containsKey(i))
+                numMap.put(i,numMap.get(i)+1);
+            else
+                numMap.put(i,1);
+        }
+
+        for (Integer key : numMap.keySet()) {
+
+            if (numMap.get(key) == 1)
+                sum += key;
+        }
+       return sum;
+
+    }
+}
